76f357e8d5ba829ed61469c0b54744e91e9b54a0
vusal8253 <131958203+vusal8253@users.noreply.github.com>
2023-10-20 17:17:31 +0400

    Create java.js

---
+++ DOM.1/Java-2/java.js
@@ -0,0 +1,12 @@
+document.querySelector('h1').innerHTML = 'Most popular Harry Potter characters';
+
+document.getElementById('fourth').innerHTML = 'Professor Snape';
+
+document.getElementsByClassName('slytherin')[0].innerHTML = 'Salazar Slytherin';
+
+document.getElementsByTagName('li')[0].innerHTML = 'Dobby';
+
+document.body.style.backgroundColor = '#201F2E';
+
+document.querySelector('.heading').style.fontFamily = 'Roboto';
+
