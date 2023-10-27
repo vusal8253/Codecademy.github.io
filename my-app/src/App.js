0fdcbbc9ffc063cf5ec5005378017bec87f46a98
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-10-27 16:58:20 +0400

    Create App.js

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Navbar } from './components/Navbar';
+import {Education} from './components/Education';
+import {Home} from './components/Home';
+import {About} from './components/About';
+import { Experience } from './components/Experience';
+
+
+export const App = () => {
+  return (
+   <div className='App'>
+      <Navbar/>
+    <Routes>
+      <Route path='/education' element={<Education/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/' element={<Home/>} />
+      <Route path='/experince' element={<Experience/>} />
+    </Routes>
+   </div>
+  );
+};
+
