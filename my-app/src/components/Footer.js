f9ac5e3d5a6a5e1621a295d71876c6b9a6b4c65c
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-10-28 12:38:13 +0400

    Update Footer.js

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -9,7 +9,8 @@ import {BsLinkedin} from 'react-icons/bs';
 
 export const Footer = () => {
   return (
-    <div className='footer'>
+    <div className='container1'>
+      <div className='footer'>
       <ul>
         <li><AiFillInstagram size='1.5rem' className = 'icons-Instagram'/></li>
         <li><BsFacebook size='1.5rem' className = 'icons-Facebook'/> </li>
@@ -17,5 +18,7 @@ export const Footer = () => {
         <li><BsLinkedin size='1.5rem' className = 'icinons-Twitter'/></li>
       </ul>
     </div>
+    </div>
+    
   )
 }
