2d3e18ee847a4660903450ce2655606c68851469
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-10-28 12:38:07 +0400

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/Navbar';
-import {Education} from './components/Education';
-import {Home} from './components/Home';
-import {About} from './components/About';
+import { Education } from './components/Education';
+import { Home } from './components/Home';
+import { About } from './components/About';
 import { Experience } from './components/Experience';
 import { Footer } from './components/Footer';
 
 
 export const App = () => {
   return (
-   <div className='App'>
-      <Navbar/>
-    <Routes>
-      <Route path='/education' element={<Education/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/' element={<Home/>} />
-      <Route path='/experince' element={<Experience/>} />
-    </Routes>
-      <Footer/>
-   </div>
+    <div className='App'>
+      <div className='wrapp'>
+        <Navbar />
+        <Routes>
+          <Route path='/education' element={<Education />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/experince' element={<Experience />} />
+        </Routes>
+        <div className='footer1'>
+          <Footer />
+        </div>
+       
+      </div>
+
+    </div>
   );
 };
 
