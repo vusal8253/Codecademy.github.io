33853140074fa7e3a57539f6cd5375270961965e
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-10-31 18:13:05 +0400

    Update About.js

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import './About.css'
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='div1'>
+      <img src="https://content.codecademy.com/projects/personal_portfolio/profile_image.jpg"></img>
+      <p className='p1'>Welcome to my personal portfolio! I am a freelance web developer with a passion for programming and coffee! Ever since I was a little kid, I loved understanding how things worked and enjoyed taking apart and reassmbling just about
+      anything I could get my hands on.</p>
+    </div>
   )
 }
