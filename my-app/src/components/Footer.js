6f0d4003a12a6a1403867a3c222719b2abcfdd8f
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-10-31 18:13:10 +0400

    Update Footer.js

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -12,10 +12,10 @@ export const Footer = () => {
     <div className='container1'>
       <div className='footer'>
       <ul>
-        <li><AiFillInstagram size='1.5rem' className = 'icons-Instagram'/></li>
-        <li><BsFacebook size='1.5rem' className = 'icons-Facebook'/> </li>
-        <li><AiOutlineTwitter size='1.5rem' className = 'icinons-Twitter'/></li>
-        <li><BsLinkedin size='1.5rem' className = 'icinons-Twitter'/></li>
+        <li><a href='https://www.instagram.com/'><AiFillInstagram size='1.7rem' className = 'icons-Instagram'/></a></li>
+        <li><a href='https://www.facebook.com/?locale=az_AZ'><BsFacebook size='1.7rem' className = 'icons-Facebook'/> </a></li>
+        <li><a href='https://twitter.com/'><AiOutlineTwitter size='1.7rem' className = 'icinons-Twitter'/></a></li>
+        <li><a href='https://www.linkedin.com/'><BsLinkedin size='1.7rem' className = 'icinons-Twitter'/></a></li>
       </ul>
     </div>
     </div>
