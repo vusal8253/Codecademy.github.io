9d9fcdf79452a537479cae88218d45ae62d8b853
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-11-02 14:48:28 +0400

    Update Experience.js

--- my-app/src/components/Experience.js
+++ my-app/src/components/Experience.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import './Experience.css';
 
 export const Experience = () => {
+  const [records, setRecords] = useState([])
+
+  useEffect(()=> {
+    axios.get('https://jsonplaceholder.typicode.com/albums/1/photos')
+     .then(res => {setRecords(res.data)})
+     .catch(err => console.log(err))
+  },[])
+
   return (
-    <div>Experience</div>
+    <div className='div2'>
+      <ul className='ul1'>
+        
+        {records.map((r,i)=>(
+          <li key={i}>
+            <td>{r.id}</td>
+            <td>.{r.title}</td>
+          </li>
+        ))}
+         
+      </ul>
+    </div>
   )
 }
