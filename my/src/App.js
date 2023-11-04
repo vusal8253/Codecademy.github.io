053700836dfb3e48c1112f74f1595844524a15ff
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-11-04 10:20:58 +0400

    Update App.js

--- my/src/App.js
+++ my/src/App.js
@@ -1,35 +1,12 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-    const cars = [
-      {
-        id: '1.', name: 'Volvo',
-      },
-      {
-        id: '2.', name: 'Hyundai',
-      },
-      {
-        id: '3.', name: 'BMW',
-      },
-      {
-        id: '4.', name: 'KIA',
-      }
-    ];
+
+   
   return (
-    <div className="App">
-    
-      {
-        cars.map((car)=>(
-          <ul>
-            <li>{car.id}</li>
-            <li>{car.name}</li>
-
-          </ul>
-        ))
-        
-
-      
-      }
+    <div>
+      <h1 className='text-center text-info'>Search filter</h1>
     </div>
   );
 }
