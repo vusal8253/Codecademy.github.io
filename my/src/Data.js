31d517ce22682358d584928c478d2ee05d047c48
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-11-06 18:04:57 +0400

    Create Data.js

---
+++ my/src/Data.js
@@ -0,0 +1,61 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
+import img6 from './img/img6.jpg';
+import img7 from './img/img7.jpg';
+import img8 from './img/img8.jpg';
+const data = {
+    cardData:[
+        {
+            id:1,
+            img:img1,
+            title:'blog 1',
+            desc:'desc 1'
+        },
+        {
+            id:2,
+            img:img2,
+            title:'blog 2',
+            desc:'desc 2'
+        },
+        {
+            id:3,
+            img:img3,
+            title:'blog 3',
+            desc:'desc 3'
+        }, 
+        {
+            id:4,
+            img:img4,
+            title:'blog 4',
+            desc:'desc 4',
+        },
+        {
+            id:5,
+            img:img5,
+            title:'blog 5',
+            desc:'desc 5'
+        },
+        {
+            id:6,
+            img:img6,
+            title:'blog 6',
+            desc:'desc 6'
+        },
+        {
+            id:7,
+            img:img7,
+            title:'blog 7',
+            desc:'desc 7'
+        }, 
+        {
+            id:8,
+            img:img8,
+            title:'blog 8',
+            desc:'desc 8',
+        }
+    ]
+}
+export default data
