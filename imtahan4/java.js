6608151d5c430e0c59d170c1ca4b2ec3137d4730
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-11-13 11:49:39 +0400

    Update java.js

---
+++ imtahan4/java.js
@@ -0,0 +1,17 @@
+function uniqueLength(nums) {
+    if(nums.length === 0) {
+        return 0;
+    }
+    let i=0;
+  return
+    while(nums[j] == nums[i]) {
+        if(let j=1; j < nums.length !==) {
+            i++;
+            nums[i] = nums[j];
+            j++;
+        } else {
+            j++;
+        }
+    }
+    nums[i] i+1;
+}
