bbc8e7f8e4e7373628940062415f4d82da195451
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-11-14 17:49:14 +0400

    Update java.js

--- imtahan4/java.js
+++ imtahan4/java.js
@@ -1,23 +1,84 @@
-const talkingPlushie = {
-    home: 'Imagination Land!',
-    composition: 'part cat, part elephant, part dolphin',
-    fur: 'Fluffy. Pink and striped.',
-      
-    greet(name) {
-      console.log(`Hello ${name}, welcome to ${this.home}`);
-    },
-    introduce() {
-      console.log(`I'm mostly cotton candy but technically. I'm ${this.composition}`);
-    },
-    spreadingJoy() {
-      console.log("BRRRR.... SPREADING JOY.... BRR.");
-    },
-    unspreadingJoy() {
-      console.log("Booting...laser eyes...acquiring target...");
-    },
-  };
-  
-  talkingPlushie.greet("Asaye");
-  talkingPlushie.talkingPlushie.name("Bing Bong");
-  remove;
-  home talkingPlushie.unspreadingJoy;
+// if (coin === "heads") {
+//   console.log('coin is heads!');
+// } else {
+//   console.log('coin is tails...');
+// }
+
+// let runTime = 35;
+// let runDistance = 3.5;
+
+// if (runTime <= 30 && runDistance > 3.5) {
+//   console.log("You're super fast!");
+// } else if (runTime >= 30 && runDistance <= 3) {
+//   console.log("You're not making your pace!");
+// } else if (runTime > 30 || runDistance > 3) {
+//   console.log("Nice workout!"); 
+// } else {
+//   console.log("Keep on running!");
+// }
+
+// walkSignal === 'Walk' ? console.log('You may walk!') : console.log('Do not walk!');
+
+// const areaOfCircle = radius => Math.PI * radius * radius;
+
+// const eatFruit = (fruit = 'apple') => {
+//   console.log(`This ${fruit} is delicious!`);
+// };
+
+// eatFruit();
+
+// const cities = ['Chicago', 'San Francisco', 'New York'];
+
+// console.log(cities[2]);
+
+// for (let i = 10; i > 0; i++) {
+//   console.log(i);
+// };
+
+// const animal = 'cat';
+
+// for (let i = 0; i < animal.length; i++) {
+//   console.log(animal[i]);
+//   for (let j = 1; j < 4; j++) {
+//     console.log(j);
+//   }
+// }
+
+// let socialMedia = ['Instagram', 'Facebook', 'Twitter'];
+
+// for (let socialMediaIndex = 0; socialMediaIndex < socialMedia.length; socialMediaIndex++) {
+//     console.log(socialMedia[socialMediaIndex]);
+// }
+
+// const nums = [1, 2, 3, 4];
+// const initializer = 90;
+
+// const addItUp = nums.reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// }, initializer);
+
+// console.log(addItUp);
+
+// const oddNumbers = [1, 3, 5, 7];
+
+// const evenNumbers = oddNumbers.map(number => {
+//   return number * 2  
+// });
+
+// const sum = (x, y) => x + y;
+// sum.alias = 'summation';
+
+// for (let x = 0; x < 10, x++) {
+//   console.log(x)
+// }
+
+// const child = document.getElementById('firstItem');
+// const orderedList = child.parentNode; 
+
+// element.addEventListener("keyup", handleKeyUp);
+
+// element.removeEventListener("keyup", handleKeyUp);
+
+// git add index.js app.js
+//git status
+
