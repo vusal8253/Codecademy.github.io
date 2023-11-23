ed0be2c656407d514441e656b18be345264150f5
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-11-23 08:26:22 +0400

    Update html.4.js

--- imtahan4/html.4.js
+++ imtahan4/html.4.js
@@ -1,44 +1,28 @@
 //(1)
-// class Artwork {
-//     constructor(title, artist, price){
-//       this._title = title;
-//       this._artist = artist;
-//       this._price = price;
-//     }
+class Artwork {
+    constructor(title, artist, price){
+      this._title = title;
+      this._artist = artist;
+      this._price = price;
+    }
   
-//     get title(){
-//       return this._title;
-//     }
+    get title(){
+      return this._title;
+    }
   
-//     get artist(){
-//       return this._artist;
-//     }
+    get artist(){
+      return this._artist;
+    }
   
-//     get price(){
-//        return this._price;
-//     }
+    get price(){
+       return this._price;
+    }
     
-    
-    
-//   }
+  }
   
-//   const starryNight = new Artwork('The Starry Night', 'Vincent van Gogh', 1300);
-//   // Set the price for starryNight
+  const starryNight = new Artwork('The Starry Night', 'Vincent van Gogh', 1300);
+  // Set the price for starryNight
   
   
-//   console.log(`${starryNight.title} by ${starryNight.artist} is on sale for $${starryNight.price}!`);
+  console.log(`${starryNight.title} by ${starryNight.artist} is on sale for $${starryNight.price}!`);
 
-//2 
-// let phoneNumber : string;
-
-// if (Math.random() > 0.5) {
-//   phoneNumber = '+61770102062';
-// } else {
-//   phoneNumber = '7167762323';
-// }
-var button = document.querySelector("button");
-
-button.addEventListener("click",function(){
-	document.body.style.backgroundColor = "red";
-	document.body.style.backgroundColor = "blue";
-});
