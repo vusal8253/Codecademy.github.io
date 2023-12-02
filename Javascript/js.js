809bba5ae4152cd486911a266404574c79b511a9
bidoluyazilim <131958203+vusal8253@users.noreply.github.com>
2023-12-02 10:04:01 +0400

    Update js.js

--- Javascript/js.js
+++ Javascript/js.js
@@ -1,4 +1,4 @@
-
+//1
 // function siyah() {
 //     document.body.style.background = 'red';  
 // }
@@ -6,7 +6,16 @@
 // let id = document.getElementById('myDiv');
 // id.addEventListener('click', siyah);
 
-
- 
+//2
+// var click
+// function siyah () {
+//     if (click) {
+//      document.body.style.background = 'red';
+//      click = false;   
+//     } else {
+//       document.body.style.background = 'blue';
+//       click = true;
+//     } 
+// }
 
 
